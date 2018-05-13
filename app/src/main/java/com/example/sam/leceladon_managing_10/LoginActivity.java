75d748fde7dc662a65caa02342164f8ee50f849f
mimizham <hamza.elansari@gmail.com>
2018-05-13 23:33:40 +0000

    developping

---
+++ app/src/main/java/com/example/sam/leceladon_managing_10/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.sam.leceladon_managing_10;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Locale;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
